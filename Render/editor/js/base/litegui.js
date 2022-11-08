5ca8397bc209cb56b1a25fa7cb496d97477f3a37
DMliangshiming <shimingliang@deepmirror.com>
2022-11-08 18:13:56 +0800

    temp

--- Render/editor/js/base/litegui.js
+++ Render/editor/js/base/litegui.js
@@ -88,20 +88,168 @@ var LiteGUI = {
 
   /**
    * 
+   * @param {*} element 
+   * @param {*} event 
+   * @param {*} callback 
+   */
+  unbind: function(element, event, callback)
+  {
+    if (element.removeEventListener)
+    {
+      element.removeEventListener(event, callback);
+    }
+    else if (element.__events && element.__events.removeEventListener)
+    {
+      element.__events.removeEventListener(event, callback);
+    }
+  },
+
+  removeClass: function(element, className)
+  {
+    var selector = "." + className;
+    var list = (element || document).querySelectorAll(selector);
+    for(var i = 0; i<list.length; i++)
+    {
+      list[i].classList.remove(className);
+    }
+  },
+
+  /**
+   * 
+   * @param {*} element 
+   * @param {*} event_name 
+   * @param {*} params 
+   * @param {*} origin 
+   */
+  trigger: function(element, event_name, params, origin)
+  {
+    var event = document.createEvent('CustomEvent');
+    event.initCustomEvent(event_name, true, true, params);
+    if (element.dispatchEvent)
+    {
+      element.dispatchEvent(event);
+    }
+    else if (element.__events)
+    {
+      element.__events.dispatchEvent(event);
+    }
+    return event;
+  },
+
+  /**
+   * 
    * @param {*} litegui_element 
    */
   add: function(litegui_element)
   {
-    this.content.appendChild(litegui_element.root || litegui_element);
+    document.body.appendChild(litegui_element);
   },
 
+  /**
+   * 
+   * @param {*} element 
+   * @returns 
+   */
   remove: function(element)
   {
     if (!element)
     {
       return;
     }
+    if (element.constructor === String)
+    {
+      var elements = document.querySelectorAll(element);
+      for (var i=0; i<elements.length; i++)
+      {
+        var element = elements[i];
+        if (element && element.parentNode)
+        {
+          element.parentNode.removeChild(element);
+        }
+      }
+    }
+    if (element.constructor === Array || element.constructor === NodeList)
+    {
+      for (var i=0; i<element.length; i++)
+      {
+        LiteGUI.remove(element[i]);
+      }
+    }
+    else if (element.root && element.root.parentNode)
+    {
+      element.root.parentNode.removeChild(element.root);
+    }
+    else if (element.parentNode)
+    {
+      element.parentNode.removeChild(element);
+    }
 
+  },
+
+  getById: function(id)
+  {
+    return document.getElementById(id);
+  },
+
+  setWindowSize: function(w, h)
+  {
+    var style = document.body.style;
+
+    if (w && h)
+    {
+      style.width = w + "px";
+      style.height = h + "px";
+      style.boxShadow = "0 0 4px black";
+      document.body.classList.remove("fullscreen");
+    }
+    else
+    {
+      if (document.body.classList.contains("fullscreen"))
+      {
+        return;
+      }
+      document.body.classList.add("fullscreen");
+      style.width = "100%";
+      style.height = "100%";
+      style.boxShadow = "0 0 0";
+    }
+    LiteGUI.trigger(LiteGUI, "resized");
+  },
+
+  maximizeWindow: function()
+  {
+    this.setWindowSize();
+  },
+
+  /**
+   * 
+   * @param {*} name 
+   */
+  setCursor: function(name)
+  {
+    document.body.style.cursor = name;
+  },
+
+  isCursorOverElement: function(event, element)
+  {
+    var left = event.pageX;
+    var top = event.pageY;
+    var rect = element.getBoundingClientRect();
+    if (!rect)
+    {
+      return false;
+    }
+    if (top > rect.top && top < (rect.top + rect.height) &&
+        left > rect.left && left < (rect.left + rect.width))
+    {
+      return true;
+    }
+    return false;
+  },
+
+  getRect: function(element)
+  {
+    return element.getBoundingClientRect();
   },
 
   newWindow: function (title, width, height, options)
