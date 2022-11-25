843fe361d870f18e8a6ec5b7b2cb8293695c2354
DMliangshiming <shimingliang@deepmirror.com>
2022-11-25 10:02:40 +0800

    temp

--- Render/editor/js/base/scene/SceneNode.js
+++ Render/editor/js/base/scene/SceneNode.js
@@ -30,3 +30,69 @@ function SceneNode(name)
     locked: false
   };
 }
+
+SceneNode.prototype.init = function (keep_components, keep_info)
+{
+
+}
+
+Object.defineProperty(SceneNode.prototype, 'name', {
+  set: function (name)
+  {
+    this.setName(name);
+  },
+  get: function ()
+  {
+    return this._name;
+  },
+  enumerable: true
+});
+
+Object.defineProperty(SceneNode.prototype, 'fullname', {
+  set: function (name)
+  {
+    throw ("You cannot set fullname because it depends on parent nodes");
+  },
+  get: function ()
+  {
+    return this.getPathName();
+  },
+  enumerable: false
+});
+
+Object.defineProperty(SceneNode.prototype, 'uid', {
+  set: function (uid)
+  {
+    if (!uid)
+    {
+      return;
+    }
+    if (uid[0] != LS._uid_prefix)
+    {
+      uid = LS._uid_prefix + uid;
+      console.warn("invalid uid, rename to : " + uid);
+    }
+
+    SceneNode._last_uid_changed = this._uid;
+
+    if (this._in_tree && this._in_tree._nodes_by_uid[this.uid])
+    {
+      delete this._in_tree._nodes_by_uid[this.uid];
+    }
+    this._uid = uid;
+    if (this._in_tree)
+    {
+      this._in_tree._nodes_by_uid[this._uid] = this;
+    }
+    LEvent.trigger(this, "uid_changed", uid);
+    if (this._in_tree)
+    {
+      LEvent.trigger(this._in_tree, "node_uid_changed", this);
+    }
+  },
+  get: function ()
+  {
+    return this._uid;
+  },
+  enumerable: true
+});
